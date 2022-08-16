b4aa4f6e640851344cbf6a7461b025b19184638c
Matheus Padilha Rodrigues <padilha.matheus@hotmail.com>
2022-08-16 17:02:15 -0300

    Spread & Rest

--- ecmaScript/resources.js
+++ ecmaScript/resources.js
@@ -42,10 +42,43 @@ const falarCom = (pessoa) => console.log('Hello ' + pessoa);
 falarCom("Joao");
 //this
 function withThis() {
-    console.log(this);
+    // console.log(this)
 }
 const withThisSpecial = withThis.bind(2);
 withThisSpecial();
 //this ??
-const ArrowThis = () => console.log(this);
-ArrowThis();
+// const ArrowThis = () => console.log(this)
+// ArrowThis()
+// Standard Parameter
+function contagemRegressiva(inicio = 2) {
+    console.log(inicio);
+    while (inicio > 0) {
+        inicio--;
+        console.log(inicio);
+    }
+    console.log("End");
+}
+console.log("========");
+contagemRegressiva(10);
+// Spread & Rest
+const numbers = [1, 10, 99, -6, 2004, 23842384];
+console.log(Math.max(...numbers));
+const TurmaA = ['Joao', 'Maria', 'Araraur', 'Soliara'];
+const TurmaB = ['Miguel', 'jorge', 'Sabrina', ...TurmaA];
+console.log(TurmaB);
+function returnArray(...args) {
+    return args;
+}
+const numeross = returnArray(1, 2, 4, 5, 7, 458, 2384);
+console.log(numeross);
+// Tuple Spred & Rest
+const tuple = [1, 'abc', false];
+function tupleParam(a, b, c) {
+    console.log(`1) ${a}${b}${c}`);
+}
+tupleParam(...tuple);
+function tupleParam2(...params) {
+    console.log(Array.isArray(params));
+    console.log(`2) ${params[0]} ${params[1]} ${params[2]}`);
+}
+tupleParam2(...tuple);
--- ecmaScript/resources.ts
+++ ecmaScript/resources.ts
@@ -54,7 +54,7 @@ falarCom("Joao")
 //this
 
 function withThis(){
-    console.log(this)
+    // console.log(this)
 }
 
 const withThisSpecial = withThis.bind(2)
@@ -63,5 +63,49 @@ withThisSpecial()
 
 //this ??
 
-const ArrowThis = () => console.log(this)
-ArrowThis()
+// const ArrowThis = () => console.log(this)
+// ArrowThis()
+
+
+// Standard Parameter
+function contagemRegressiva(inicio: number = 2): void {
+    console.log(inicio)
+    while(inicio>0){
+        inicio--
+        console.log(inicio)
+    }
+    console.log("End")
+}
+console.log("========")
+contagemRegressiva(10)
+
+// Spread & Rest
+
+const numbers = [1, 10, 99, -6, 2004, 23842384]
+console.log(Math.max(...numbers))
+
+const TurmaA: string[] = ['Joao', 'Maria', 'Araraur', 'Soliara']
+const TurmaB: string[] = ['Miguel', 'jorge', 'Sabrina',...TurmaA]
+console.log(TurmaB)
+
+function returnArray(...args: number[]):number[]{
+    return args
+}
+
+const numeross = returnArray(1, 2, 4, 5, 7, 458, 2384)
+console.log(numeross)
+
+// Tuple Spred & Rest
+const tuple: [number, string, boolean]  = [1, 'abc', false]
+
+function tupleParam(a:number, b:string, c:boolean): void {
+    console.log(`1) ${a}${b}${c}`)
+}
+tupleParam(...tuple)
+
+function tupleParam2(...params: [number, string, boolean]){
+    console.log(Array.isArray(params))
+    console.log(`2) ${params[0]} ${params[1]} ${params[2]}`)
+}
+
+tupleParam2(...tuple)
